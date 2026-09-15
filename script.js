4938f53fe6e318074ab75fc2470d6bb8009c3d03
Cristina <cnh9746@mavs.uta.edu>
2026-09-15 17:27:39 -0500

    added f-16

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const items = [
     {img: "assets/f-22.jpeg" , text: "F-22", info: "F-22 Raptor is a fifth-generation stealth air superiority fighter. It entered into servie in December of 2005, and is manufactured by lockheed martin"},
-    {img: "assets/f-19.jpeg" , text: "F-19", info: "F-19 is a fictional stealth fighter that was widely belived to exist in the 1980s"}
+    {img: "assets/f-19.jpeg" , text: "F-19", info: "F-19 is a fictional stealth fighter that was widely belived to exist in the 1980s"},
+    {img: "assets/f-16.jpeg" , text: "F-16", info: "F-16 Fighting Falcom is a single-enginer, supersontic multirole fighter aircraft. It was developed by General Dynamics. It entered service in 1978. Top Speed is Mach 2.0+"}
 ];
 const button = document.getElementById("Btn");
 const randomIndex = Math.floor(Math.random() * items.length);
